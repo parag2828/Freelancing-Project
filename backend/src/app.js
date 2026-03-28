ec50f5dadc3c47f8c6a3c09dc91448b921113974
Arpan Dhandar <arpandhandar.77@gmail.com>
2026-03-29 01:18:11 +0530

    add CORS middleware and update package dependencies

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,11 @@
 import express from "express"
+import cors from "cors"
 
-export const app = express();
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+export default app 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv"
-import { app } from "./app.js"
+import  app  from "./app.js"
 import connectDB from "./db/db.js";
 
 dotenv.config({
